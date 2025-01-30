7e545a9c2a42de9ce2d65d206d1427cce64ea512
Andy Castle <masterchief_and@live.com>
2025-01-30 10:45:21 -0500

    Se creo realizo el servicio de countries y se mostro los datos de las regiones en el selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,11 +9,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [],
 })
 export class SelectorPageComponent {
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
     borders: ['', Validators.required],
   });
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 }
